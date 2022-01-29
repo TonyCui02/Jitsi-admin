7041931444c8bcb62322a80f4228dd18a4d8bcc1
Tony Cui <Tonycui02@gmail.com>
2022-01-29 22:49:58 +1300

    Setup frontend template

---
+++ client/src/layouts/HomePageLayout.js
@@ -0,0 +1,15 @@
+import { Box } from "@mui/system";
+import React from "react";
+import TopNavbar from "../components/TopNavbar";
+
+
+const HomePageLayout = () => {
+  return (
+    <div>
+      <TopNavbar />
+      <Box>test</Box>
+    </div>
+  );
+};
+
+export default HomePageLayout;
---
+++ client/src/views/HomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HomePageLayout from "../layouts/HomePageLayout";
+
+const HomePage = () => {
+  return <HomePageLayout />;
+};
+
+export default HomePage;
