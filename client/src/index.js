0104eb3a198ffae46965a250a44a2a2b5fdec8f2
Tony Cui <Tonycui02@gmail.com>
2022-04-09 16:12:05 +1200

    added auth

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
