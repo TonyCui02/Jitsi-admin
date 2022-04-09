9091c75168e8d2fa988374a9bb63db0a4663bfcf
Tony Cui <Tonycui02@gmail.com>
2022-04-09 17:28:51 +1200

    change file structure to remove subfolders

--- server/src/models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { mongo } from "mongoose";
-
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String,
-})
-
-export const userModel = mongoose.model('user', userSchema);
